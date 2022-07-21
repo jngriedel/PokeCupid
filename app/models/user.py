e0142bb7278a87286cef559befe8a4e8ea85a2c5
jngriedel <jngriedel@gmail.com>
2022-07-21 13:58:22 -0700

    models updated

--- app/models/user.py
+++ app/models/user.py
@@ -1,23 +1,28 @@
 from .db import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+import datetime
 
 
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(40), nullable=False, unique=True)
+    name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(255), nullable=False, unique=True)
-    hashed_password = db.Column(db.String(255), nullable=False)
+    gender = db.Column(db.String(10), nullable=False)
+    bio = db.Column(db.String(2000), nullable=False)
+    hashedPassword = db.Column(db.String(255), nullable=False)
+    pokemonId = db.Column(db.Integer, nullable=False)
+    createdAt = db.Column(db.DateTime, default=datetime.datetime.now(), nullable=False)
 
     @property
     def password(self):
-        return self.hashed_password
+        return self.hashedPassword
 
     @password.setter
     def password(self, password):
-        self.hashed_password = generate_password_hash(password)
+        self.hashedPassword = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
@@ -25,6 +30,9 @@ class User(db.Model, UserMixin):
     def to_dict(self):
         return {
             'id': self.id,
-            'username': self.username,
-            'email': self.email
+            'name': self.name,
+            'email': self.email,
+            'gender': self.gender,
+            'bio': self.bio,
+            'pokemonId': self.pokemonId
         }
--- app/seeds/users.py
+++ app/seeds/users.py
@@ -4,15 +4,24 @@ from app.models import db, User
 # Adds a demo user, you can add other users here if you want
 def seed_users():
     demo = User(
-        username='Demo', email='demo@aa.io', password='password')
-    marnie = User(
-        username='marnie', email='marnie@aa.io', password='password')
-    bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password')
+        name='Ash', email='demo@aa.io', password='password', gender='Male' , bio= 'I wanna be the very best, like no one ever was.', pokemonId=25)
+    Brock = User(
+        name='Brock', email='brock@aa.io', password='password', gender='Male' , bio= 'Looking for the Nurse Joy to my Brock', pokemonId=95)
+    Misty = User(
+        name='Misty', email='misty@aa.io', password='password', gender='Female' , bio= 'No bug pokemon or I swear I will scream', pokemonId=120)
+
+    Officer_Jenny = User(
+        name='Jenny', email='jenny@aa.io', password='password', gender='Female' , bio= 'Law enforcement and avid Bike rider', pokemonId=59)
+
+    Nurse_Joy = User(
+        name='Joy', email='joy@aa.io', password='password', gender='Female' , bio= 'I heal and care for sick pokemon. Tired of thirsty Gym Leaders blowing up my DMs. Please just act normal.', pokemonId=113)
+
+    Surge = User(
+        name='Surge', email='surge@aa.io', password='password', gender='Male' , bio= 'Meet up with me. I seem intimidating but I may shock you in a good way.', pokemonId=26)
 
     db.session.add(demo)
-    db.session.add(marnie)
-    db.session.add(bobbie)
+    db.session.add(Brock)
+    db.session.add(Misty)
 
     db.session.commit()
 
