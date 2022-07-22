8390538f70cbca715e53aef57c2b8665acc6f520
Moe <moe62197@gmail.com>
2022-07-22 17:50:28 -0400

    initiating questionnaire

---
+++ react-app/src/components/Questionnaire.js
@@ -0,0 +1,110 @@
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux'
+import { Redirect } from 'react-router-dom';
+
+
+const Questionnaire = () => {
+    const [answer1, setAnswer1] = useState(null)
+    const [answer2, setAnswer2] = useState(null)
+    const [answer3, setAnswer3] = useState(null)
+    const [answer4, setAnswer4] = useState(null)
+    const [answer5, setAnswer5] = useState(null)
+    const [answer6, setAnswer6] = useState(null)
+    const [answer7, setAnswer7] = useState(null)
+    const [answer8, setAnswer8] = useState(null)
+    const [answer9, setAnswer9] = useState(null)
+    const [answer10, setAnswer10] = useState(null)
+    const [answer11, setAnswer11] = useState(null)
+    const [answer12, setAnswer12] = useState(null)
+    const [answer13, setAnswer13] = useState(null)
+    const [answer14, setAnswer14] = useState(null)
+    const [answer15, setAnswer15] = useState(null)
+    const [answer16, setAnswer16] = useState(null)
+    const [answer17, setAnswer17] = useState(null)
+    const [answer18, setAnswer18] = useState(null)
+    const [answer19, setAnswer19] = useState(null)
+    const [answer20, setAnswer20] = useState(null)
+
+    const addAnswer1 = (e) => {
+        setAnswer1(e.target.value);
+    }
+
+    const addAnswer2 = (e) => {
+        setAnswer2(e.target.value);
+    }
+
+    const addAnswer3 = (e) => {
+        setAnswer3(e.target.value);
+    }
+
+    const addAnswer4 = (e) => {
+        setAnswer4(e.target.value);
+    }
+
+    const addAnswer5 = (e) => {
+        setAnswer5(e.target.value);
+    }
+
+    const addAnswer6 = (e) => {
+        setAnswer6(e.target.value);
+    }
+
+    const addAnswer7 = (e) => {
+        setAnswer7(e.target.value);
+    }
+
+    const addAnswer8 = (e) => {
+        setAnswer8(e.target.value);
+    }
+
+    const addAnswer9 = (e) => {
+        setAnswer9(e.target.value);
+    }
+
+    const addAnswer10 = (e) => {
+        setAnswer10(e.target.value);
+    }
+
+    const addAnswer11 = (e) => {
+        setAnswer11(e.target.value);
+    }
+
+    const addAnswer12 = (e) => {
+        setAnswer12(e.target.value);
+    }
+
+    const addAnswer13 = (e) => {
+        setAnswer13(e.target.value);
+    }
+
+    const addAnswer14 = (e) => {
+        setAnswer14(e.target.value);
+    }
+
+    const addAnswer15 = (e) => {
+        setAnswer15(e.target.value);
+    }
+
+    const addAnswer16 = (e) => {
+        setAnswer16(e.target.value);
+    }
+
+    const addAnswer17 = (e) => {
+        setAnswer17(e.target.value);
+    }
+
+    const addAnswer18 = (e) => {
+        setAnswer18(e.target.value);
+    }
+
+    const addAnswer19 = (e) => {
+        setAnswer19(e.target.value);
+    }
+
+    const addAnswer20 = (e) => {
+        setAnswer20(e.target.value);
+    }
+    return (
+        null
+    );
+}
