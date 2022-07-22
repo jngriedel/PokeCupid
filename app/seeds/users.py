c559e556d729a204dfc9102fe900633d88c4e409
jngriedel <jngriedel@gmail.com>
2022-07-22 13:05:08 -0700

    fixed env and everything is seeded

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -4,20 +4,20 @@ from app.models import db, User
 # Adds a demo user, you can add other users here if you want
 def seed_users():
     demo = User(
-        name='Ash', email='demo@aa.io', password='password', gender='Male' , bio= 'I wanna be the very best, like no one ever was.', pokemonId=25)
+        id =1, name='Ash', email='demo@aa.io', password='password', gender='Male' , bio= 'I wanna be the very best, like no one ever was.', pokemonId=25)
     Brock = User(
-        name='Brock', email='brock@aa.io', password='password', gender='Male' , bio= 'Looking for the Nurse Joy to my Brock', pokemonId=95)
+        id =2, name='Brock', email='brock@aa.io', password='password', gender='Male' , bio= 'Looking for the Nurse Joy to my Brock', pokemonId=95)
     Misty = User(
-        name='Misty', email='misty@aa.io', password='password', gender='Female' , bio= 'No bug pokemon or I swear I will scream', pokemonId=120)
+        id =3, name='Misty', email='misty@aa.io', password='password', gender='Female' , bio= 'No bug pokemon or I swear I will scream', pokemonId=120)
 
     Officer_Jenny = User(
-        name='Jenny', email='jenny@aa.io', password='password', gender='Female' , bio= 'Law enforcement and avid Bike rider', pokemonId=59)
+        id =4, name='Jenny', email='jenny@aa.io', password='password', gender='Female' , bio= 'Law enforcement and avid Bike rider', pokemonId=59)
 
     Nurse_Joy = User(
-        name='Joy', email='joy@aa.io', password='password', gender='Female' , bio= 'I heal and care for sick pokemon. Tired of thirsty Gym Leaders blowing up my DMs. Please just act normal.', pokemonId=113)
+        id =5, name='Joy', email='joy@aa.io', password='password', gender='Female' , bio= 'I heal and care for sick pokemon. Tired of thirsty Gym Leaders blowing up my DMs. Please just act normal.', pokemonId=113)
 
     Surge = User(
-        name='Surge', email='surge@aa.io', password='password', gender='Male' , bio= 'Meet up with me. I seem intimidating but I may shock you in a good way.', pokemonId=26)
+        id =6, name='Surge', email='surge@aa.io', password='password', gender='Male' , bio= 'Meet up with me. I seem intimidating but I may shock you in a good way.', pokemonId=26)
 
     db.session.add(demo)
     db.session.add(Brock)
