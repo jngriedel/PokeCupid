45767ed226ede40d849f99c4f581630e60c9d56a
jngriedel <jngriedel@gmail.com>
2022-07-23 11:45:29 -0700

    working on likes

--- app/api/matches_routes.py
+++ app/api/matches_routes.py
@@ -7,23 +7,33 @@ from app.models import User, Match, db
 matches_routes = Blueprint('matches', __name__)
 
 
-@matches_routes.route('/<int:id>', methods=['POST'])
+@matches_routes.route('/', methods=['POST'])
 @login_required
-def add_match(id):
+def add_match():
     data = request.json
-    userId = data[userId]
-    userId2 = data[userId2]
-    db.session.delete(to_delete)
-    db.session.commit()
+    liker = data['liker']
+    liked = data['liked']
 
-    return {'imageId': id}
+    possibleLike = Match.query.filter(Match.userId == liked, Match.userId2 == liker)
+    print('**************************************',possibleLike)
+    print(possibleLike[0])
+    if possibleLike:
+        possibleLike[0]['matched'] = True
+
+        db.session.commit()
+        return {'match' : possibleLike.to_dict()}
+
+    else:
+        new_Match = Match(userId=liker, userId2 = liked)
+        db.session.add(new_Match)
+        db.session.commit()
+        return {'matchId': new_Match['id']}
 
 @matches_routes.route('/<int:id>', methods=['DELETE'])
 @login_required
 def unmatch(id):
-    to_edit = ProfileImage.query.get(id)
-    data = request.json
-    to_edit.title = data['title']
+    to_delete = Match.query.get(id)
+    db.session.delete(to_delete)
     db.session.commit()
 
-    return {'image': to_edit.to_dict()}
+    return {'matchId': id}
---
+++ react-app/src/store/matches.js
@@ -0,0 +1,110 @@
+// constants
+const ADD_MATCH = 'session/ADD_MATCH'
+const REMOVE_MATCH = 'session/REMOVE_MATCH';
+
+const GET_MATCHES = 'session/SET_MATCHES'
+
+const addMatch = (match) => ({
+  type: ADD_MATCH,
+  payload: match
+});
+
+const removeMatch = (matchId) => ({
+  type: REMOVE_MATCH,
+  payload: matchId
+})
+
+const getMatches = (matches) => ({
+    type: GET_MATCHES,
+    payload: matches
+})
+
+
+
+
+const initialState = { matches: null };
+
+
+
+export const getUserMatches = (userId) => async (dispatch) => {
+
+  const response = await fetch(`/api/matches/${userId}`);
+
+  if (response.ok) {
+    const data = await response.json();
+
+    dispatch(getMatches(data.matches))
+    return null;
+  } else if (response.status < 500) {
+    const data = await response.json();
+    if (data.errors) {
+      return data.errors;
+    }
+  } else {
+    return ['An error occurred. Please try again.']
+  }
+
+}
+export const newMatch = (liker, liked) => async (dispatch) => {
+    const response = await fetch(`/api/matches/`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            liker,
+            liked
+          })
+        })
+        if (response.ok) {
+        const data = await response.json();
+        if (data.matchId) dispatch(addMatch(data.match))
+        return null;
+      }
+      else {
+        return ['An error occurred. Please try again.']
+      }
+}
+
+export const unMatch = (matchId) => async (dispatch) => {
+    const response = await fetch(`/api/matches/${matchId}`, {
+    method: 'DELETE'}
+    )
+    if (response.ok) {
+    const data = await response.json();
+
+    dispatch(removeMatch(data.matchId))
+    return null;
+  }
+  else {
+    return ['An error occurred. Please try again.']
+  }
+
+}
+
+
+
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    case ADD_MATCH: {
+      const newState = {...state}
+      newState[action.payload.id] = action.payload
+      return newState
+    }
+    case REMOVE_MATCH: {
+      const newState = {...state}
+      delete newState[action.payload]
+      return newState
+    }
+    case GET_MATCHES: {
+        const newState = {}
+        action.payload.forEach((match)=>{
+            newState[match.id] = match
+        })
+        return newState
+    }
+
+    default:
+      return state;
+  }
+}
