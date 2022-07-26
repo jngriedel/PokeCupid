22c740945e5015c9f17f4e3e6a77cb7a32f748c0
Moe <moe62197@gmail.com>
2022-07-26 18:47:48 -0400

    halp

--- react-app/src/components/EditProfileAnswers.js
+++ react-app/src/components/EditProfileAnswers.js
@@ -3,7 +3,27 @@ import { useHistory } from 'react-router-dom'
 import { useDispatch } from 'react-redux';
 
 const EditProfileAnswers = () => {
+    const [showEditMaster, setShowEditMaster] = useState(false)
+    const [showEditSub, setShowEditSub] = useState(false)
+
+    const handleEditSubmit = () => {
+        setShowEditMaster(true)
+    }
+
+    useEffect(()=>{
+        if (setShowEditMaster) {
+            setShowEditSub(true)
+        }
+    })
+
+
     return (
+        <>
+        {showEditMaster && (
+            <div>
+                <button onClick={handleEditSubmit}>Edit</button>
+            </div>)}
+        </>
         
     );
 }
