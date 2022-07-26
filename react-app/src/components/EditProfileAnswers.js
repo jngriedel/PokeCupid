181d7537ab2a0518ca67201b5557cb37790b1e83
Moe <moe62197@gmail.com>
2022-07-26 15:42:23 -0400

    initiating edit-profile component

---
+++ react-app/src/components/EditProfileAnswers.js
@@ -0,0 +1,11 @@
+import { useState, useEffect } from 'react'
+import { useHistory } from 'react-router-dom'
+import { useDispatch } from 'react-redux';
+
+const EditProfileAnswers = () => {
+    return (
+        
+    );
+}
+
+export default EditProfileAnswers
